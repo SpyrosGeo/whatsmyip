b416918acf15779c67099eb066a97d275d71b186
SpyrosGeo <xarodasimba@gmail.com>
2020-01-23 21:19:16 +0200

    added project to github

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 #!/usr/bin/env node
 const axios = require('axios');
-const coords ={};
-const positionAPI =`http://gd.geobytes.com/GetCityDetails`;
+const coords = {};
+const chalk = require('chalk');
+const positionAPI = `http://ifconfig.me/`;
 
 
-function getPosition(){
-    return axios.get(positionAPI);
+function getPosition() {
+  return axios.get(positionAPI);
 }
-  
-
-  getPosition().then((res)=>{
-    
-    coords.ip = res.data.geobytesremoteip;
-     return coords;
-       
-   }).catch((res)=>{
-       console.log(res)
-   }).then((coords)=>{
-      returnedCords(coords);
-   })
 
 
-   const returnedCords = (coords)=>{
-    let {ip} = coords 
-    console.log(ip)
-   }
-   
+getPosition().then((res) => {
+  coords.ip = res.data
+  return coords;
+
+}).catch((res) => {
+  console.log(res)
+}).then((coords) => {
+  returnedCords(coords);
+})
+
+
+const returnedCords = (coords) => {
+  let { ip } = coords
+  console.log(chalk.bgBlack.white(ip))
+}
